2204a6f28a9391cfd7a82173bbffd8a0e2f39d46
Suphakit P. <me@suphakit.net>
2026-05-16 15:54:20 +0700

    chore<config>: change config to class

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,32 @@
-export const host = "./dist";
+// import "dotenv/config";
+
+// export const host = process.env.HOST ?? "./dist";
+// export const homepage = process.env.HOMEPAGE ?? "https://suphakit.net";
+// export const author = process.env.AUTHOR ?? "Suphakit P.";
+
+export class ConfigClass {
+  public host: string;
+  public homepage: string;
+  public author: string;
+
+  constructor() {
+    this.host = "./dist";
+    this.homepage = "https://suphakit.net";
+    this.author = "Suphakit P.";
+  }
+
+  public setHost(host: string) {
+    this.host = host;
+    localStorage.setItem("host", host);
+  }
+
+  public setHomepage(homepage: string) {
+    this.homepage = homepage;
+    localStorage.setItem("homepage", homepage);
+  }
+
+  public setAuthor(author: string) {
+    this.author = author;
+    localStorage.setItem("author", author);
+  }
+}
