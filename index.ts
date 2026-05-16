f362f7e43cc6628ceb4799119eceeebb97677745
Suphakit P. <me@suphakit.net>
2026-05-16 15:36:58 +0700

    feat<modules>: setting up build tool

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+const res = await Bun.build({
+  entrypoints: ["./src/index.ts", "./src/style.css"],
+  outdir: "./dist",
+  minify: { whitespace: true, identifiers: true },
+  format: "esm",
+});
+
+if (res.success) {
+  console.log(`Build Successfully`);
+  console.log(`Files in dist folder`);
+} else {
+  console.error(`Failed to build.. check your code and try again..`);
+}
